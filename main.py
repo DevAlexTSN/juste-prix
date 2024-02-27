91a9a300ee3b9bb9ad3085fba61fd62ba3b588c0
AlexTSN <alextsndev@gmail.com>
2024-02-27 15:19:36 +0100

    ajout du compteur de coups et du calcul du score

--- main.py
+++ main.py
@@ -2,24 +2,36 @@ import random
 
 #Choix du nom
 
-name = input("Quel est votre nom ?")
-print("Bonjour,"+ name +"!. Jouons ensemble! ")
+# name = input("Quel est votre nom ?")
+# print("Bonjour, "+ name +"!. Jouons ensemble! ")
 
 #Choix par le jeu de la variable prix ( valeur entière entre 1 et 20)
 
 random_1 = int(input("Choisis la valeur basse du prix "))
 random_2 = int(input("Choisis la valeur haute du prix "))
 
+count = 0
+score = random_2-random_1
+
 prix = random.randint(random_1,random_2)
-# print(prix) #le print prix sera a supprimer
+print(prix) #le print prix sera a supprimer
 
 while True : 
     choix=int(input("Quel est le prix de l'objet ?  " ))
     if choix == prix : 
-        print('vous avez gagné')
+        print(count)
+        count = count+1
+        score_final = score-count
+        print("vous avez gagné en " + str(count) + " coups !")
+        print("Votre score est de " +str(score_final)+ " points sur  " + str(score)+ "!")
         break
     elif choix < prix :
+        count = count+1
+        score_final = score-count
         print("c'est plus!")
+        
     else : 
+        count = count+1
+        score_final = score-count
         print("c'est moins!")
 
