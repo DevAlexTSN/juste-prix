d297c8fda5c02b6953da064c1f379b37b8e37b2c
AlexTSN <alextsndev@gmail.com>
2024-02-27 14:50:11 +0100

    save

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import random
 
 #Choix du nom
 
-# name = input("Quel est votre nom ?")
-# print("Bonjour,"+ name +"!. Jouons ensemble! ")
+name = input("Quel est votre nom ?")
+print("Bonjour,"+ name +"!. Jouons ensemble! ")
 
 #Choix par le jeu de la variable prix ( valeur entière entre 1 et 20)
 
@@ -11,10 +11,10 @@ random_1 = int(input("Choisis la valeur basse du prix "))
 random_2 = int(input("Choisis la valeur haute du prix "))
 
 prix = random.randint(random_1,random_2)
-print(prix) #le print prix sera a supprimer
+# print(prix) #le print prix sera a supprimer
 
 while True : 
-    choix=int(input("Quel est le prix de l'objet ?" ))
+    choix=int(input("Quel est le prix de l'objet ?  " ))
     if choix == prix : 
         print('vous avez gagné')
         break
